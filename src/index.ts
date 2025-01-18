16eb2fbb9f282660b9fd15945b29f792988dff9c
Sky Leite <sky@leite.dev>
2025-01-18 15:05:35 -0300

    Add parseFile utility

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Tokenizer } from "./tokenizer/tokenizer";
 import { TextDocument } from "vscode-languageserver-textdocument";
+import { readFile } from "node:fs/promises";
 
-export default async function tokenize(content: string) {
+export async function parse(content: string) {
     const document = TextDocument.create("file://my.rpy", "rpy", 0, content);
     return { document, nodes: await Tokenizer.tokenizeDocument(document) };
 }
+
+export async function parseFile(path: string) {
+    const file = await readFile(path, "utf8");
+    return parse(file);
+}
+
+export default parse;
